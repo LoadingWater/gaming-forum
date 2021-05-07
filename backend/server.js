36ac398acd77e6a6eb8871b51d420a1332e63e17
Hidden Pool <storm.kpl@gmail.com>
2021-05-07 17:57:43 +0200

    Added default route

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose')
+const routes = require('./routes/login.route.js')
 
 require ('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || PORT;
 
 app.use(cors());
 app.use(express.json());
 
+app.use(routes)
+app.use("*", (req, res) => {
+    res.status(404).json({error: "Not found"})
+})
+
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true} );
+mongoose.connect(
+    uri, 
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, wtimeout:2500 } )
+    .catch(err => {
+        console.error(err.stack)
+        process.exit
+    })
+    .then(async => {
+        app.listen(port, () => {
+            console.log(`Server is runnig on port: ${port}`);
+        })
+    });
 
 const connection = mongoose.connection;
 connection.once('open', () => 
@@ -19,7 +36,3 @@ connection.once('open', () =>
     console.log('MongoDB database connection established successfully');
 })
 
-
-app.listen(port, () => {
-    console.log(`Server is runnig on port: ${port}`);
-})
