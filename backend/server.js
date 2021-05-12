f778551f8093d7525eebdfb2587121bfb709fefa
Hidden Pool <storm.kpl@gmail.com>
2021-05-12 12:33:42 +0200

    Added dependencies for login/signup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose')
 const routes = require('./routes/login.route.js')
-const airbnbDAO = require('./dao/airbnbDAO.js')
 
 require ('dotenv').config();
 
@@ -18,28 +17,14 @@ app.use("*", (req, res) => {
 })
 
 const uri = process.env.ATLAS_URI;
-const sampleUir = process.env.ATLAS_SAMPLE_URI;
 mongoose.connect(
-    sampleUir, 
+    uri, 
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, wtimeout:2500 } )
     .catch(err => {
         console.error(err.stack)
         process.exit
     })
     .then(async clientArg => {
-
-        // client from lambda didn't work
-        const client = mongoose.connection.getClient();
-        const collection = await client.db("sample_airbnb").collection("listingsAndReviews")
-        //console.log(client)
-        try
-        {
-            await airbnbDAO.injectDB(collection)
-        }
-        catch(e)
-        {
-            console.error(`Airbnb db injection error: ${e}`)
-        }
         app.listen(port, () => {
             console.log(`Server is runnig on port: ${port}`);
         })
